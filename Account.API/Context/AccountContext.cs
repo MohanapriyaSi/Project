8deeb0fbbf354db1c20c065d9fe10554d82b3ffd
MohanapriyaSi <88383992+MohanapriyaSi@users.noreply.github.com>
2021-08-24 15:16:25 +0530

    Add files via upload

---
+++ Account.API/Context/AccountContext.cs
@@ -0,0 +1,19 @@
+﻿using Account.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Account.API.Context
+{
+    public class AccountContext:DbContext,IAccountContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<AccountDetails> Account { get; set; }
+        
+    }
+}
