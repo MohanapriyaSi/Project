d73cdc60a7513c50cf96308a6c8486ff818ac830
MohanapriyaSi <88383992+MohanapriyaSi@users.noreply.github.com>
2021-08-24 15:31:10 +0530

    Add files via upload

---
+++ Transaction.API/Models/DepositAndWithdraw.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transaction.API.Models
+{
+    public class DepositAndWithdraw
+    {
+        public ulong AccountId { get; set; }
+        public double? Amount { get; set; }
+        public string AccountType { get; set; }
+        public string TransactionRequest { get; set; }
+
+    }
+}
---
+++ Transaction.API/Models/UserTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transaction.API.Models
+{
+    public class UserTransaction
+    {
+        [Key]
+        public int recordId { get; set; }
+        public int CustomerId { get; set; }
+        public ulong CurrentAccountId { get; set; }
+        public ulong SavingsAccountId { get; set; }
+        public double? CurrentBalance { get; set; }
+        public double? SavingsBalance { get; set; }
+        public double? TotalAccountBalance { get; set; }
+    }
+}
