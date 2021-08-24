8d2933e28891fe3195989d63630207e79bffb2ad
MohanapriyaSi <88383992+MohanapriyaSi@users.noreply.github.com>
2021-08-24 07:42:40 +0530

    Add files via upload

---
+++ Customer.API/Context/CustomerContext.cs
@@ -0,0 +1,18 @@
+﻿using Customer.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.API.Context
+{
+    public class CustomerContext:DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> context) : base(context)
+        {
+
+        }
+        public DbSet<CustomerDetails> Customer { get; set; }
+    }
+}
